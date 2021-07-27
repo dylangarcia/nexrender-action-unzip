d385136555873db3b0adad3e96ac3ea1ac76d542
Dylan Garcia <dylanga98@gmail.com>
2021-07-27 19:22:32 -0400

    Fix settings variable

--- index.js
+++ index.js
@@ -9,15 +9,15 @@ module.exports = (job, settings, action, type) => {
     );
   }
 
-  if (!settings.output) {
-    throw new Error('No settings.output value was provided.');
+  if (!action.output) {
+    throw new Error('No action.output value was provided.');
   }
 
   return new Promise((resolve, reject) => {
     fs.createReadStream(job.template.dest)
       .pipe(unzipper.Extract({ path: job.workpath }))
       .on("close", () => {
-        job.template.dest = settings.output;
+        job.template.dest = action.output;
         resolve();
       })
       .on("error", (err) => {
