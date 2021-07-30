f13354a865dd9745ce0de923cdd4cdf72a31fbf2
Dylan Garcia <dylanga98@gmail.com>
2021-07-30 17:39:07 -0400

    Fix path

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const fs = require("fs");
+const path = require("path");
 const unzipper = require("unzipper");
 const { name } = require("./package.json");
 
@@ -10,14 +11,17 @@ module.exports = (job, settings, action, type) => {
   }
 
   if (!action.output) {
-    throw new Error('No action.output value was provided.');
+    throw new Error("No action.output value was provided.");
   }
 
   return new Promise((resolve, reject) => {
     fs.createReadStream(job.template.dest)
       .pipe(unzipper.Extract({ path: job.workpath }))
       .on("close", () => {
-        job.template.dest = action.output;
+        job.template.dest = path.join(
+          job.workpath,
+          path.basename(action.output)
+        );
         resolve();
       })
       .on("error", (err) => {
